0cbf380cb67330f8a478a597901803f3dc4ecc2c
Trupti Kandalkar <truptikandalkar0@gmail.com>
2026-09-02 14:54:03 +0530

    Json and api

---
+++ Javascript/JSONandAPI/app.js
@@ -0,0 +1,6 @@
+let JSONres = '{"name": "Trupti Kandalkar","age": 21,"email": "truptikandalkar0@gmail.com","skills": ["HTML", "CSS", "JavaScript", "React"],"isStudent": true}';
+
+console.log(JSONres);
+
+let resultObj = JSON.parse(JSONres);
+console.log(resultObj);
